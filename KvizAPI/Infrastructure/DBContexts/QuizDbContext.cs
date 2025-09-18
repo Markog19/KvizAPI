99347c956e3a4b6a6039548488ddf78cea0e7e4c
Markog19 <markog19@gmail.com>
2025-09-18 20:55:56 +0200

    Added DBContext

---
+++ KvizAPI/Infrastructure/DBContexts/QuizDbContext.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using KvizAPI.Domain.Entities;
+
+namespace KvizAPI.Infrastructure.DBContexts
+{
+    public class QuizDbContext(DbContextOptions<QuizDbContext> options) : Microsoft.EntityFrameworkCore.DbContext(options)
+    {
+        public DbSet<Quiz> Quizzes { get; set; } = null!;
+        public DbSet<Question> Questions { get; set; } = null!;
+        public DbSet<QuestionQuiz> QuestionQuizzes { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure composite key for join table
+            modelBuilder.Entity<QuestionQuiz>()
+                .HasKey(qt => new { qt.QuestionId, qt.QuizId });
+
+            // Configure relationships
+            modelBuilder.Entity<QuestionQuiz>()
+                .HasOne(qt => qt.Question)
+                .WithMany(q => q.QuestionQuizzes)
+                .HasForeignKey(qt => qt.QuestionId);
+
+            modelBuilder.Entity<QuestionQuiz>()
+                .HasOne(qt => qt.Quiz)
+                .WithMany(qz => qz.QuestionQuizzes)
+                .HasForeignKey(qt => qt.QuizId);
+        }
+    }
+}
