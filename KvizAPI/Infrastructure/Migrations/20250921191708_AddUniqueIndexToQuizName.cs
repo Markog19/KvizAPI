c9121f350e7f10db67d6374a4b4afe5c0ad77ad7
Markog19 <markog19@gmail.com>
2025-09-21 21:31:10 +0200

    added migration for making name unique

---
+++ KvizAPI/Infrastructure/Migrations/20250921191708_AddUniqueIndexToQuizName.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KvizAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueIndexToQuizName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Quizzes_Name",
+                table: "Quizzes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Quizzes",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Quizzes_Name",
+                table: "Quizzes",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Quizzes_Name",
+                table: "Quizzes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Quizzes",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Quizzes_Name",
+                table: "Quizzes",
+                column: "Name");
+        }
+    }
+}
